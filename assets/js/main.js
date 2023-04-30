32c391d4ef8eb4473e3c3a64cf9626a5d1874e89
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-04-30 11:39:33 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// Potrfolio
+
+const time = 3000;
+const step = 1;
+
+function outNum(num, elem) {
+  let l = document.querySelector('#' + elem);
+  let n = 0;
+  let t = Math.round(time / (num / step));
+  let interval = setInterval(() => {
+    n = n + step;
+    if (n == num) {
+      clearInterval(interval);
+    }
+    l.innerHTML = n;
+  },
+    t);
+}
+
+outNum(875, 'out-1');
+
+
